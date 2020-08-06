0e76e3efba7e284cc6a80cee58eddf96f64ac1cc
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-06 13:10:34 +0900

    wip add race

---
+++ app/admin/assets/js/add_race.js
@@ -0,0 +1,6 @@
+(function(){
+  const event_table_for_edit_div = document.querySelector('div#event_table_for_edit')
+  const result_div = document.querySelector('div#event_add_result')
+  const submit_button = document.querySelector('button#submit_race')
+
+})();
