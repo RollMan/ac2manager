22f246d5e512326278a6ea054d530594a2aa8ff1
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-07 11:49:35 +0900

    fix small bugs

--- app/static/static/header.js
+++ app/static/static/header.js
@@ -11,7 +11,7 @@ const body = `
             <a href="/">Top</a>
             <a href="/about.html">About</a>
             <a href="/pastevents.html">Past Events</a>
-            <a href="admin/admin.html">Admin Area</a>
+            <a href="/admin/admin.html">Admin Area</a>
           </nav>
         </header>
 
