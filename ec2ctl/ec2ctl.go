6e995f388008985e0eeedc39987f208556200d14
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-08 15:04:01 +0900

    initial commit

---
+++ ec2ctl/ec2ctl.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "time"
+)
+
+func main(){
+  for {
+    time.Sleep(1 * time.Second)
+  }
+}
---
+++ ec2ctl/go.mod
@@ -0,0 +1,3 @@
+module github.com/RollMan/ac2manager/ec2ctl
+
+go 1.14
