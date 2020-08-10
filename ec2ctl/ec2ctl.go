b1678f262d8b81623ffafcb649f9f6043b1a74cd
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-10 13:00:42 +0900

    wip

--- ec2ctl/ec2ctl.go
+++ ec2ctl/ec2ctl.go
@@ -5,16 +5,51 @@ import (
   "log"
 )
 
+type WakeupKind int
+
+const (
+  Small = iota
+  Medium
+  Large
+)
+
 func main(){
   log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
   time.Local = time.FixedZone("GMT", 0)
   prev := time.Now()
   for {
-    now := time.Now()
-    sleep_by := now.Add(time.Minute * time.Duration(1)).Truncate(time.Minute)
-    sleepUntilNextMinute(sleep_by)
+    var now time.Time
+    for {
+      sleep_by := prev.Add(time.Minute * time.Duration(1)).Truncate(time.Minute)
+      sleepUntilNextMinute(sleep_by)
 
-    prev = now
+      now = time.Now()
+      now_unixminute := int(now.Unix() / 60)
+      prev_unixminute := int(now.Unix() / 60)
+      if now_unixminute != prev_unixminute {
+        break
+      }
+    }
+
+    timeDiff := now.Sub(prev)
+    timeDiffMinute := (int)timeDiff.Minutes()
+
+    if timeDiffMinute == 1 {
+      prev = now
+      find_jobs(prev)
+    }else{
+      now_unixminute := int(now.Unix() / 60)
+
+      for {
+        prev++
+        find_jobs(prev)
+        prev_unixminute := int(prev.Unix() / 60)
+        if !(prev_unixminute < now_unixminute) {
+          break
+        }
+      }
+    }
+    runQueue()
   }
 }
 
