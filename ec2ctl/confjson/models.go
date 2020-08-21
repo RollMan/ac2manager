25843c186fefc6b6f389524390da87158dd304e3
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-22 08:34:21 +0900

    head capital

--- ec2ctl/confjson/models.go
+++ ec2ctl/confjson/models.go
@@ -1,121 +1,120 @@
 package confjson
 
 import (
-  "io/ioutil"
-  "encoding/json"
-  "os"
-  "log"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 type AssistRules struct {
-  disableIdealLine         int `json:"disableIdealLine"`
-  disableAutosteer         int `json:"disableAutosteer"`
-  stabilityControlLevelMax int `json:"stabilityControlLevelMax"`
-  disableAutoPitLimiter    int `json:"disableAutoPitLimiter"`
-  disableAutoGear          int `json:"disableAutoGear"`
-  disableAutoClutch        int `json:"disableAutoClutch"`
-  disableAutoEngineStart   int `json:"disableAutoEngineStart"`
-  disableAutoWiper         int `json:"disableAutoWiper"`
-  disableAutoLights        int `json:"disableAutoLights"`
+	DisableIdealLine         int `json:"disableIdealLine"`
+	DisableAutosteer         int `json:"disableAutosteer"`
+	StabilityControlLevelMax int `json:"stabilityControlLevelMax"`
+	DisableAutoPitLimiter    int `json:"disableAutoPitLimiter"`
+	DisableAutoGear          int `json:"disableAutoGear"`
+	DisableAutoClutch        int `json:"disableAutoClutch"`
+	DisableAutoEngineStart   int `json:"disableAutoEngineStart"`
+	DisableAutoWiper         int `json:"disableAutoWiper"`
+	DisableAutoLights        int `json:"disableAutoLights"`
 }
 
-func (t *AssistRules) ParseConfig(filename string){
-  jsonFile, err := os.Open(filename)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  defer jsonFile.Close()
+func (t *AssistRules) ParseConfig(filename string) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  json.Unmarshal([]byte(byteValue), t)
+	json.Unmarshal([]byte(byteValue), t)
 }
-
 
 type Settings struct {
-    serverName                 string     `json:"serverName"`
-    adminPassword              string     `json:"adminPassword"`
-    carGroup                   string     `json:"carGroup"`
-    trackMedalsRequirement     int        `json:"trackMedalsRequirement"`
-    safetyRatingRequirement    int        `json:"safetyRatingRequirement"`
-    racecraftRatingRequirement int        `json:"racecraftRatingRequirement"`
-    password                   string     `json:"password"`
-    maxCarSlots                int        `json:"maxCarSlots"`
-    spectatorPassword          string     `json:"spectatorPassword"`
-    configVersion              int        `json:"configVersion"`
+	ServerName                 string `json:"serverName"`
+	AdminPassword              string `json:"adminPassword"`
+	CarGroup                   string `json:"carGroup"`
+	TrackMedalsRequirement     int    `json:"trackMedalsRequirement"`
+	SafetyRatingRequirement    int    `json:"safetyRatingRequirement"`
+	RacecraftRatingRequirement int    `json:"racecraftRatingRequirement"`
+	Password                   string `json:"password"`
+	MaxCarSlots                int    `json:"maxCarSlots"`
+	SpectatorPassword          string `json:"spectatorPassword"`
+	ConfigVersion              int    `json:"configVersion"`
 }
 
-func (t *Settings) ParseConfig(filename string){
-  jsonFile, err := os.Open(filename)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  defer jsonFile.Close()
+func (t *Settings) ParseConfig(filename string) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  json.Unmarshal([]byte(byteValue), t)
+	json.Unmarshal([]byte(byteValue), t)
 }
 
 type Session struct {
-  hourOfDay               int   `json:"hourOfDay"`
-  dayOfWeekend            int   `json:"dayOfWeekend"`
-  timeMultiplier          int   `json:"timeMultiplier"`
-  sessionType             int   `json:"sessionType"`
-  sessionDurationMinutes  int   `json:"sessionDurationMinutes"`
+	HourOfDay              int `json:"hourOfDay"`
+	DayOfWeekend           int `json:"dayOfWeekend"`
+	TimeMultiplier         int `json:"timeMultiplier"`
+	SessionType            int `json:"sessionType"`
+	SessionDurationMinutes int `json:"sessionDurationMinutes"`
 }
 
-func (t *Session) ParseConfig(filename string){
-  jsonFile, err := os.Open(filename)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  defer jsonFile.Close()
+func (t *Session) ParseConfig(filename string) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  json.Unmarshal([]byte(byteValue), t)
+	json.Unmarshal([]byte(byteValue), t)
 }
 
 type Event struct {
-          track                                              string
-          preRaceWaitingTimeSeconds                          int
-          sessionOverTimeSeconds                             int
-          ambientTemp                                        int
-          cloudLevel                                         float32
-          rain                                               float32
-          weatherRandomness                                  int
-          sessions                                           []Session
-          configVersion                                      int
+	Track                     string    `json:"track"`
+	PreRaceWaitingTimeSeconds int       `json:"preRaceWaitingTimeSeconds"`
+	SessionOverTimeSeconds    int       `json:"sessionOverTimeSeconds"`
+	AmbientTemp               int       `json:"ambientTemp"`
+	CloudLevel                float32   `json:"cloudLevel"`
+	Rain                      float32   `json:"rain"`
+	WeatherRandomness         int       `json:"weatherRandomness"`
+	Sessions                  []Session `json:"sessions"`
+	ConfigVersion             int       `json:"configVersion"`
 }
 
-func (t *Event) ParseConfig(filename string){
-  jsonFile, err := os.Open(filename)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  defer jsonFile.Close()
+func (t *Event) ParseConfig(filename string) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  json.Unmarshal([]byte(byteValue), t)
+	json.Unmarshal([]byte(byteValue), t)
 }
 
 type Configuration struct {
-    udpPort         int `json:"udpPort"`
-    tcpPort         int `json:"tcpPort"`
-    maxConnections  int `json:"maxConnections"`
-    configVersion   int `json:"configVersion"`
+	UdpPort        int `json:"udpPort"`
+	TcpPort        int `json:"tcpPort"`
+	MaxConnections int `json:"maxConnections"`
+	ConfigVersion  int `json:"configVersion"`
 }
 
-func (t *Configuration) ParseConfig(filename string){
-  jsonFile, err := os.Open(filename)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  defer jsonFile.Close()
+func (t *Configuration) ParseConfig(filename string) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  json.Unmarshal([]byte(byteValue), t)
+	json.Unmarshal([]byte(byteValue), t)
 }
