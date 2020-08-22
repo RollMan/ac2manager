71a95fc952e90f288b56c4ebd5b5aae97351ccbf
Yohei Shimmyo <qwpmb554@gmail.com>
2020-08-22 11:27:29 +0900

    load config

--- ec2ctl/jobmng/jobmng.go
+++ ec2ctl/jobmng/jobmng.go
@@ -1,41 +1,51 @@
 package jobmng
 
 import (
-  "time"
-  "log"
-  "github.com/RollMan/ac2manager/ec2ctl/db"
-  "github.com/RollMan/ac2manager/app/models"
-  "github.com/RollMan/ac2manager/ec2ctl/confjson"
+	"github.com/RollMan/ac2manager/app/models"
+	"github.com/RollMan/ac2manager/ec2ctl/confjson"
+	"github.com/RollMan/ac2manager/ec2ctl/db"
+	"log"
+	"time"
 )
 
 var queue []models.Event
 
-func InitQueue(){
-  queue = make([]models.Event, 0)
+func InitQueue() {
+	queue = make([]models.Event, 0)
 }
 
 func FindJobs(t time.Time) {
-  targetInMinute := t.Truncate(time.Minute)
-  events := selectJobsByDate(targetInMinute)
-  for _, e := range events {
-    queue = append(queue, e)
-  }
+	targetInMinute := t.Truncate(time.Minute)
+	events := selectJobsByDate(targetInMinute)
+	for _, e := range events {
+		queue = append(queue, e)
+	}
 }
 
-func selectJobsByDate(t time.Time){
-  var events []models.Event
-  t1 := t
-  t2 := t.Add(time.Minute)
-  _, err := db.DbMap.Select(&events, "SELECT * FROM events WHERE CONVERT(?, DATETIME) <= events.startdate and events.startdate < CONVERT(?, DATETIME)", t1, t2)
+func selectJobsByDate(t time.Time) {
+	var events []models.Event
+	t1 := t
+	t2 := t.Add(time.Minute)
+	_, err := db.DbMap.Select(&events, "SELECT * FROM events WHERE CONVERT(?, DATETIME) <= events.startdate and events.startdate < CONVERT(?, DATETIME)", t1, t2)
 
-  if err != nil {
-    log.Fatal(err)
-  }
-  return events
+	if err != nil {
+		log.Fatal(err)
+	}
+	return events
 }
 
-func RunQueue(){
-  // TODO
-  // create configure json
-  // deploy server
+func RunQueue() {
+	virtualQueue := make([]models.Event, len(queue))
+	copy(virtualQueue, queue)
+	queue = make([]models.Event, 0)
+
+	go RunInstanse(virtualQueue)
+}
+
+func RunInstanse(virtualQueue []models.Event) {
+	for _, q := range virtualQueue {
+		assistRules, settings, event, configuration, eventRules := confjson.ReadDefaultConfigs()
+		confjson.SetConfigs(q, &assistRules, &settings, &event, &configuration, &eventRules)
+		// TODO
+	}
 }
