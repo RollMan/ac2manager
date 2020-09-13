5693df58f1f1ef9720ad245504d71ba045fe0c6e
Yohei Shimmyo <qwpmb554@gmail.com>
2020-09-13 21:44:19 +0900

    wip: jobmng test

--- ec2ctl/jobmng/jobmng_test.go
+++ ec2ctl/jobmng/jobmng_test.go
@@ -44,3 +44,62 @@ func TestSelectJobsByDate(t *testing.T) {
 		}
 	}
 }
+
+func TestFindJobs(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("Error %s occuered when opening db mock", err)
+	}
+	dbMap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{}}
+	dbMap.AddTableWithName(models.Event{}, "events").SetKeys(true, "id")
+	defer dbMap.Db.Close()
+
+	empty_rows := sqlmock.NewRows([]string{"id", "startdate"})
+
+	time1 := time.Date(2020, 5, 3, 23, 0, 0, 0, time.UTC)
+	time2 := time.Date(2020, 5, 5, 11, 30, 0, 0, time.UTC)
+
+	cases := []struct {
+		description string
+		rows        *sqlmock.Rows
+		time        time.Time
+		expected    []JobQueue
+	}{
+		{
+			description: "Test when current time matches an event",
+			rows:        empty_rows.AddRow(3, time1),
+			time:        time1,
+			expected:    []JobQueue{{JobType: Start, Event: models.Event{Id: 3, Startdate: time1}}},
+		},
+		{
+			description: "Test when current time matches plural events",
+			rows:        empty_rows.AddRow(0, time1).AddRow(5, time1),
+			time:        time1,
+			expected:    []JobQueue{{JobType: Start, Event: models.Event{Id: 0, Startdate: time1}}, {JobType: Start, Event: models.Event{Id: 5, Startdate: time1}}},
+		},
+		{
+			description: "Test when current time does not maches any events",
+			rows:        empty_rows.AddRow(0, time1),
+			time:        time2,
+			expected:    []JobQueue{{}},
+		},
+	}
+
+	for _, c := range cases {
+		mock.ExpectQuery(`SELECT \* FROM events`).
+			WithArgs(c.time, c.time.Add(time.Minute)).
+			WillReturnRows(c.rows)
+
+		res := FindJobs(c.time, []JobQueue{}, dbMap)
+
+		if len(res) != len(c.expected) {
+			t.Errorf("Error in a test.\ntestcase description: %s\nThe number of result %d must %d but not.\nres:%v\nexpected:%v\n", c.description, len(res), len(c.expected), res, c.expected)
+		}
+
+		for i, _ := range res {
+			if res[i] != c.expected[i] {
+				t.Errorf("Error in a test.\ntestcase description: %s\nThe result unmatch.\nres:%v\nexpected:%v\n", c.description, res[i], c.expected[i])
+			}
+		}
+	}
+}
