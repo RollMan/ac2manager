4d68d89cffd02916dbd45733bf9e511f8a1a5daf
Yohei Shimmyo <qwpmb554@gmail.com>
2020-09-20 12:14:19 +0900

    fix errors

--- game_server/usr/local/bin/launch_acc_server.sh
+++ game_server/usr/local/bin/launch_acc_server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+### Address of destination
+IP_ADDRESS=
+
 ### Fetch and place configuration JSONs
 INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-rsync -r -e "ssh -i ~/.ssh/id_rsa" admin@${INSTANCE_ID}:/opt/ac2manager/${INSTANCE_ID} ~/server/cfg
+rsync -r -e "ssh -i ~/.ssh/id_rsa" "admin@${IP_ADDRESS}:/opt/ac2manager/${INSTANCE_ID}/*" ~/server/cfg
 
 ### Launch
-wine /home/admin/server/accServer.exe
+exec wine /home/ubuntu/server/accServer.exe
