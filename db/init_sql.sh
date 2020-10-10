993ea651c62725867aa04164630f2c063ebaf4c0
Yohei Shimmyo <qwpmb554@gmail.com>
2020-10-10 20:01:19 +0900

    change db column of id into BINARY(16)

--- db/init_sql.sh
+++ db/init_sql.sh
@@ -14,7 +14,7 @@ CREATE TABLE users (
 INSERT INTO users (userid, pwhash, attribute) VALUES ('${AC2_APP_ADMINUSERNAME}', '${AC2_APP_ADMINPASSWORD_HASHED}', 1);
 
 CREATE TABLE events (
-  id INT(10) unsigned not null auto_increment primary key,
+  id BINARY(16) not null primary key,
   startdate DATETIME not null,
   track VARCHAR(64),
   weatherRandomness INT,
@@ -36,5 +36,5 @@ CREATE TABLE events (
   tyreSetCount INT
 );
 
-INSERT into events (startdate, track, weatherRandomness, P_hourOfDay, P_timeMultiplier, P_sessionDurationMinute, Q_hourOfDay, Q_timeMultiplier, Q_sessionDurationMinute, R_hourOfDay, R_timeMultiplier, R_sessionDurationMinute, pitWindowLengthSec, isRefuellingAllowedInRace, mandatoryPitstopCount, isMandatoryPitstopRefuellingRequired, isMandatoryPitstopTyreChangeRequired, isMandatoryPitstopSwapDriverRequired, tyreSetCount) VALUES ('2000-01-01 11:30:00', 'zandvoort_2019', 1, 12, 1, 10, 13, 1, 10, 14, 1, 10, 120, true, 1, false, true, true, 3);" | mysql -u root --password=${MYSQL_ROOT_PASSWORD}
+INSERT into events (id, startdate, track, weatherRandomness, P_hourOfDay, P_timeMultiplier, P_sessionDurationMinute, Q_hourOfDay, Q_timeMultiplier, Q_sessionDurationMinute, R_hourOfDay, R_timeMultiplier, R_sessionDurationMinute, pitWindowLengthSec, isRefuellingAllowedInRace, mandatoryPitstopCount, isMandatoryPitstopRefuellingRequired, isMandatoryPitstopTyreChangeRequired, isMandatoryPitstopSwapDriverRequired, tyreSetCount) VALUES (0x0fba3964131745a9b8f4a9f762ba05f8, '2000-01-01 11:30:00', 'zandvoort_2019', 1, 12, 1, 10, 13, 1, 10, 14, 1, 10, 120, true, 1, false, true, true, 3);" | mysql -u root --password=${MYSQL_ROOT_PASSWORD}
  echo "ABCDEFG====================================="
